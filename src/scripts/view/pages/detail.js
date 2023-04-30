856ae9f7e80ee88e0f0186b25b18236b5b517709
Erick Winata <erickwinz27@gmail.com>
2023-04-30 13:14:07 +0700

    Penambahan IDB dan Tombol Like

--- src/scripts/view/pages/detail.js
+++ src/scripts/view/pages/detail.js
@@ -1,11 +1,13 @@
 import RestaurantSource from '../../data/restaurant-source';
 import UrlParser from '../../routes/url-parser';
+import LikeButtonInitiator from '../../utils/like-button-initiator';
 import { createRestaurantDetailTemplate } from '../templates/template-creator';
 
 const Detail = {
   async render() {
     return `
       <div id="restaurant" class="restaurant"></div>
+      <div id="likeButtonContainer"></div>
         `;
   },
 
@@ -13,8 +15,19 @@ const Detail = {
     const url = UrlParser.parseActiveUrlWithoutCombiner();
     const restaurant = await RestaurantSource.detailRestaurant(url.id);
     const restaurantContainer = document.querySelector('#restaurant');
+
     restaurantContainer.innerHTML = createRestaurantDetailTemplate(restaurant);
-    console.log(restaurant);
+
+    LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      restaurant: {
+        id: restaurant.id,
+        title: restaurant.name,
+        overview: restaurant.description,
+        backdrop_path: restaurant.address,
+        vote_average: restaurant.rating,
+      },
+    });
 
     // TODO: tampilkan movie di dalam DOM
   },
--- src/scripts/view/templates/template-creator.js
+++ src/scripts/view/templates/template-creator.js
@@ -32,7 +32,21 @@ const createRestaurantDetailTemplate = (restaurant) => `
     </div>
 `;
 
+const createLikeButtonTemplate = () => `
+  <button aria-label="like this restaurant" id="likeButton" class="like">
+     <i class="fa fa-heart-o" aria-hidden="true"></i>
+  </button>
+`;
+
+const createLikedButtonTemplate = () => `
+  <button aria-label="unlike this restaurant" id="likeButton" class="like">
+    <i class="fa fa-heart" aria-hidden="true"></i>
+  </button>
+`;
+
 export {
   createRestaurantDetailTemplate,
   createRestaurantItemTemplate,
+  createLikeButtonTemplate,
+  createLikedButtonTemplate,
 };
